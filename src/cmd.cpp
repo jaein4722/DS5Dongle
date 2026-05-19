0d19e5611e8cb17a7941eb2e9aa1adfbadf83056
Gamejoongsa <jane10000@cau.ac.kr>
2026-05-19 22:36:46 +0900

    Add UF2 bootloader command

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -12,6 +12,7 @@
 #include "battery_status.h"
 #include "config.h"
 #include "device/usbd.h"
+#include "pico/bootrom.h"
 #include "pico/time.h"
 
 bool is_pico_cmd(uint8_t report_id) {
@@ -73,7 +74,8 @@ void pico_cmd_set(uint8_t report_id, uint8_t const *buffer, uint16_t bufsize) {
 
     // 0x01 update config in variable
     // 0x02 write config to flash
-    // 0x03 reconnect tinyusb device;
+    // 0x03 reconnect tinyusb device
+    // 0x04 enter UF2 bootloader
     if (buffer[0] == 0x01) {
         printf("[CMD] Enter config set func\n");
         set_config(buffer + 1, bufsize - 1);
@@ -87,5 +89,11 @@ void pico_cmd_set(uint8_t report_id, uint8_t const *buffer, uint16_t bufsize) {
         tud_disconnect();
         sleep_ms(150);
         tud_connect();
+    }
+    if (buffer[0] == 0x04) {
+        printf("[CMD] Enter UF2 bootloader\n");
+        tud_disconnect();
+        sleep_ms(150);
+        reset_usb_boot(0, 0);
     }
 }
